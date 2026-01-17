0e25d4752d9cada5c3329ffb4fbd764a349f4314
aiximerada <74672082+aiximerada@users.noreply.github.com>
2026-01-17 21:19:12 +0800

    金鑰更改

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,29 +1,22 @@
 // js/firebase-config.js
-// Firebase 核心連線設定 (含資料庫與驗證系統)
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-// 新增引入 Auth 相關功能
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } 
-    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs } 
-    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAo6on0f2QUbPaRfxpIEb2uvhKxDvPUvs",
-  authDomain: "yulubox.firebaseapp.com",
-  projectId: "yulubox",
-  storageBucket: "yulubox.firebasestorage.app",
-  messagingSenderId: "256466567852",
-  appId: "1:256466567852:web:233b74668908ffcb9b9509",
-  measurementId: "G-YPCY41EKWX"
+  apiKey: "AIzaSyAAo6on0f2QUbPaRfxpIEb2uvhKxDvPUvs",
+  authDomain: "yulubox.firebaseapp.com",
+  projectId: "yulubox",
+  storageBucket: "yulubox.firebasestorage.app",
+  messagingSenderId: "256466567852",
+  appId: "1:256466567852:web:233b74668908ffcb9b9509",
+  measurementId: "G-YPCY41EKWX"
 };
 
-// 初始化
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app); // 初始化驗證系統
+const auth = getAuth(app);
 
-// 匯出功能給其他檔案使用
 export { 
     db, auth, 
     collection, addDoc, query, orderBy, limit, getDocs,
